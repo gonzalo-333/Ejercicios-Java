3fd0457cc27f23cb734c486983839b18892b7b0c
Gonzalo <mgs.argentum@gmail.com>
2025-08-12 17:26:35 -0300

    Add files via upload

---
+++ FuncionesVoid.java
@@ -0,0 +1,31 @@
+
+public class FuncionesVoid {
+    
+    public static void main(String[] args){
+        // como solo imprime un valor, no retorna nada,
+        // no lo guardo en ninguna variable
+        imprimirSimbolo('x', 5);
+        imprimirSimbolo('@', 8);
+        imprimirSimbolo('3', 3);
+
+        // como la funcion devuelve un valor,
+        // lo guardo en una variable y la imprimo
+        String s = sucesionSimbolos('#', 7);
+        System.out.println(s);
+    }
+    
+    static void imprimirSimbolo(char simbolo, int veces){
+        for(int i = 0; i < veces; i++){
+            System.out.print(simbolo);
+        }
+        System.out.println();
+    }
+    
+    static String sucesionSimbolos(char simbolo, int veces){
+        String sucesion = "";
+        for(int i = 0; i < veces; i++){
+            sucesion += simbolo;
+        }
+        return sucesion;
+    }
+}
